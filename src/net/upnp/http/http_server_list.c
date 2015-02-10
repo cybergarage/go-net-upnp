309d39628712d05f5641f2c62f7b2e568d9bde84
Satoshi Konno <skonno@cybergarage.org>
2015-02-10 10:30:55 +0900

    * Initial import from mupnp for C.

---
+++ src/net/upnp/http/http_server_list.c
@@ -0,0 +1,186 @@
+/******************************************************************
+ *
+ * mUPnP for C
+ *
+ * Copyright (C) The go-net-upnp Authors 2005
+ * Copyright (C) 2006 Nokia Corporation. All rights reserved.
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <mupnp/http/http.h>
+#include <mupnp/net/interface.h>
+#include <mupnp/util/log.h>
+
+/****************************************
+* mupnp_http_serverlist_new
+****************************************/
+
+mUpnpHttpServerList *mupnp_http_serverlist_new()
+{
+	mUpnpHttpServerList *httpServerList;
+
+	mupnp_log_debug_l4("Entering...\n");
+
+	httpServerList = (mUpnpHttpServerList *)malloc(sizeof(mUpnpHttpServerList));
+
+	if ( NULL != httpServerList )
+	{
+		mupnp_list_header_init((mUpnpList *)httpServerList);
+		
+		httpServerList->sock = NULL;
+		httpServerList->acceptThread = NULL;
+		httpServerList->listener = NULL;
+		httpServerList->userData = NULL;
+		httpServerList->mutex = NULL;
+	}
+	
+	mupnp_log_debug_l4("Leaving...\n");
+
+	return httpServerList;
+}
+
+/****************************************
+* mupnp_http_serverlist_delete
+****************************************/
+
+void mupnp_http_serverlist_delete(mUpnpHttpServerList *httpServerList)
+{
+	mupnp_log_debug_l4("Entering...\n");
+
+	mupnp_http_serverlist_clear(httpServerList);
+	free(httpServerList);
+
+	mupnp_log_debug_l4("Leaving...\n");
+}
+
+/****************************************
+* mupnp_http_serverlist_open
+****************************************/
+
+BOOL mupnp_http_serverlist_open(mUpnpHttpServerList *httpServerList, int port)
+{
+	mUpnpNetworkInterfaceList *netIfList;
+	mUpnpNetworkInterface *netIf;
+	mUpnpHttpServer *httpServer;
+	char *bindAddr;
+	BOOL result =FALSE;
+	
+	mupnp_log_debug_l4("Entering...\n");
+
+	netIfList = mupnp_net_interfacelist_new();
+#ifndef CG_NET_USE_ANYADDR
+	mupnp_net_gethostinterfaces(netIfList);
+#else
+	netIf = mupnp_net_interface_getany();
+	mupnp_net_interfacelist_add(netIfList, netIf);
+#endif
+	for (netIf = mupnp_net_interfacelist_gets(netIfList); netIf; netIf = mupnp_net_interface_next(netIf)) {
+		bindAddr = mupnp_net_interface_getaddress(netIf);
+		if (mupnp_strlen(bindAddr) <= 0)
+			continue;
+		httpServer = mupnp_http_server_new();
+
+		if (mupnp_http_server_open(httpServer, port, bindAddr) == FALSE) {
+			/* Theo Beisch - why break off, 
+			   we may be running ok on at least some IF??? 
+			   (at least WINCE does...) */
+			continue;
+		}
+		mupnp_http_serverlist_add(httpServerList, httpServer);
+		result = TRUE; //at least one IF found ok
+	}
+
+	
+	if (result==FALSE) mupnp_http_serverlist_clear(httpServerList);
+	mupnp_net_interfacelist_delete(netIfList);
+
+	return result;	
+}
+
+/****************************************
+* mupnp_http_serverlist_close
+****************************************/
+
+BOOL mupnp_http_serverlist_close(mUpnpHttpServerList *httpServerList)
+{
+	mUpnpHttpServer *httpServer;
+	
+	mupnp_log_debug_l4("Entering...\n");
+
+	for (httpServer = mupnp_http_serverlist_gets(httpServerList); httpServer != NULL; httpServer = mupnp_http_server_next(httpServer))
+		mupnp_http_server_close(httpServer);
+
+	return TRUE;
+
+	mupnp_log_debug_l4("Leaving...\n");
+}
+
+/****************************************
+* mupnp_http_serverlist_start
+****************************************/
+
+BOOL mupnp_http_serverlist_start(mUpnpHttpServerList *httpServerList)
+{
+	mUpnpHttpServer *httpServer;
+	
+	mupnp_log_debug_l4("Entering...\n");
+
+	for (httpServer = mupnp_http_serverlist_gets(httpServerList); httpServer != NULL; httpServer = mupnp_http_server_next(httpServer))
+		mupnp_http_server_start(httpServer);
+	
+	mupnp_log_debug_l4("Leaving...\n");
+
+	return TRUE;
+}
+
+/****************************************
+* mupnp_http_serverlist_stop
+****************************************/
+
+BOOL mupnp_http_serverlist_stop(mUpnpHttpServerList *httpServerList)
+{
+	mUpnpHttpServer *httpServer;
+	
+	mupnp_log_debug_l4("Entering...\n");
+
+	for (httpServer = mupnp_http_serverlist_gets(httpServerList); httpServer != NULL; httpServer = mupnp_http_server_next(httpServer))
+		mupnp_http_server_stop(httpServer);
+		
+	mupnp_log_debug_l4("Leaving...\n");
+
+	return TRUE;
+}
+
+/****************************************
+* mupnp_http_serverlist_setlistener
+****************************************/
+
+void mupnp_http_serverlist_setlistener(mUpnpHttpServerList *httpServerList, CG_HTTP_LISTENER listener)
+{
+	mUpnpHttpServer *httpServer;
+	
+	mupnp_log_debug_l4("Entering...\n");
+
+	for (httpServer = mupnp_http_serverlist_gets(httpServerList); httpServer != NULL; httpServer = mupnp_http_server_next(httpServer))
+		mupnp_http_server_setlistener(httpServer, listener);
+
+	mupnp_log_debug_l4("Leaving...\n");
+}
+
+/****************************************
+* mupnp_http_serverlist_setuserdata
+****************************************/
+
+void mupnp_http_serverlist_setuserdata(mUpnpHttpServerList *httpServerList, void *value)
+{
+	mUpnpHttpServer *httpServer;
+	
+	mupnp_log_debug_l4("Entering...\n");
+
+	for (httpServer = mupnp_http_serverlist_gets(httpServerList); httpServer != NULL; httpServer = mupnp_http_server_next(httpServer))
+		mupnp_http_server_setuserdata(httpServer, value);
+
+	mupnp_log_debug_l4("Leaving...\n");
+}
---
+++ src/net/upnp/net/net_function.c
@@ -0,0 +1,88 @@
+/******************************************************************
+ *
+ * mUPnP for C
+ *
+ * Copyright (C) The go-net-upnp Authors 2005
+ * Copyright (C) 2006 Nokia Corporation. All rights reserved.
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include <mupnp/net/interface.h>
+#include <mupnp/util/log.h>
+
+/****************************************
+* global variables
+****************************************/
+
+static BOOL CG_NET_USE_ONLY_IPV4_ADDR = FALSE;
+static BOOL CG_NET_USE_ONLY_IPV6_ADDR = FALSE;
+
+////////////////////////////////////////////////
+//	mupnp_net_isuseaddress
+////////////////////////////////////////////////
+
+BOOL mupnp_net_isuseaddress(char *addr)
+{
+	mupnp_log_debug_l4("Entering...\n");
+
+	if (CG_NET_USE_ONLY_IPV6_ADDR == TRUE) {
+		if (mupnp_net_isipv6address(addr) == FALSE)
+			return FALSE;
+	}
+	if (CG_NET_USE_ONLY_IPV4_ADDR == TRUE) {
+		if (mupnp_net_isipv6address(addr) == TRUE)
+			return FALSE;
+	}
+	return TRUE;
+
+	mupnp_log_debug_l4("Leaving...\n");
+}
+
+////////////////////////////////////////////////
+//	mupnp_net_getipv6address
+////////////////////////////////////////////////
+
+BOOL mupnp_net_isipv6address(const char *addr)
+{
+	mupnp_log_debug_l4("Entering...\n");
+
+	if (addr == NULL)
+		return FALSE;
+	if (0 < mupnp_strchr(addr, ":", 1))
+		return TRUE;
+	return FALSE;
+
+	mupnp_log_debug_l4("Leaving...\n");
+}
+
+////////////////////////////////////////////////
+//	mupnp_net_getipv6address
+////////////////////////////////////////////////
+
+int mupnp_net_getipv6scopeid(const char *addr)
+{
+	size_t addrLen;
+	ssize_t perIdx;
+	char scopeIDBuf[8+1];
+
+	mupnp_log_debug_l4("Entering...\n");
+
+	if (mupnp_net_isipv6address(addr) == FALSE)
+		return 0;
+	addrLen = mupnp_strlen(addr);
+	perIdx = mupnp_strchr(addr, "%", 1);
+	if (perIdx < 0)
+		return 0;
+	mupnp_strncpy(scopeIDBuf, (addr+perIdx+1), sizeof(scopeIDBuf)-1);
+	scopeIDBuf[sizeof(scopeIDBuf)-1] = '\0';
+	
+	mupnp_log_debug_l4("Leaving...\n");
+	
+	return atoi(scopeIDBuf);
+}
