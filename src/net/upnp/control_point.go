4af9e3df60f301caf2b57cd60569c87bd4c36aea
Satoshi Konno <skonno@cybergarage.org>
2015-06-16 20:55:52 +0900

    * Updated control point.

--- src/net/upnp/control_point.go
+++ src/net/upnp/control_point.go
@@ -4,14 +4,42 @@
 
 package upnp
 
+import (
+	"net/upnp/ssdp"
+)
+
 // A ControlPoint represents a clinet.
 type ControlPoint struct {
 	RootDevices []Device
+	SSDPServer *ssdp.SSDPServer
 }
 
 // NewControlPoint returns a new Client.
 func NewControlPoint() *ControlPoint {
 	cp := &ControlPoint{}
 	cp.RootDevices = make([]Device, 0)
+	cp.SSDPServer = ssdp.NewSSDPServer()
 	return cp
+}
+
+// Start starts this control point.
+func (self *ControlPoint) Start() error {
+	self.SSDPServer.Listener = self
+	err := self.SSDPServer.Start()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Stop stops this control point.
+func (self *ControlPoint) Stop() error {
+	err := self.SSDPServer.Stop()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (self *ControlPoint) DeviceNotifyReceived(ssdpPkt *ssdp.SSDPPacket) {
 }
