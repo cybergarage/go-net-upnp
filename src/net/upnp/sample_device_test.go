85ec2d09e62a6ae8daf0f2ae53eae03f42f1f6c9
Satoshi Konno <skonno@cybergarage.org>
2015-07-10 15:22:30 +0900

    * Updated device tests.

---
+++ src/net/upnp/sample_device_test.go
@@ -0,0 +1,95 @@
+// Copyright 2015 The go-net-upnp Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package upnp
+
+import (
+	"encoding/xml"
+)
+
+type sampleDevice struct {
+	*Device
+}
+
+func NewSampleDevice() (*sampleDevice, error) {
+	dev, err := NewDeviceFromDescription(binaryLightDeviceDescription)
+	if err != nil {
+		return nil, err
+	}
+
+	service, err := dev.GetServiceByType("urn:schemas-upnp-org:service:SwitchPower:1")
+	if err != nil {
+		return nil, err
+	}
+
+	err = service.LoadDescriptionString(switchPowerServiceDescription)
+	if err != nil {
+		return nil, err
+	}
+
+	sampleDev := &sampleDevice{Device: dev}
+
+	return sampleDev, nil
+}
+
+const binaryLightDeviceDescription = xml.Header +
+	"<root>" +
+	"  <device>" +
+	"    <serviceList>" +
+	"      <service>" +
+	"        <serviceType>urn:schemas-upnp-org:service:SwitchPower:1</serviceType>" +
+	"        <serviceId>urn:upnp-org:serviceId:SwitchPower.1</serviceId>" +
+	"      </service>" +
+	"    </serviceList>" +
+	"  </device>" +
+	"</root>"
+
+const switchPowerServiceDescription = xml.Header +
+	"<scpd>" +
+	"  <serviceStateTable>" +
+	"    <stateVariable>" +
+	"      <name>Target</name>" +
+	"      <sendEventsAttribute>no</sendEventsAttribute> " +
+	"      <dataType>boolean</dataType>" +
+	"      <defaultValue>0</defaultValue>" +
+	"    </stateVariable>" +
+	"    <stateVariable>" +
+	"      <name>Status</name>" +
+	"      <dataType>boolean</dataType>" +
+	"      <defaultValue>0</defaultValue>" +
+	"    </stateVariable>" +
+	"  </serviceStateTable>" +
+	"  <actionList>" +
+	"    <action>" +
+	"    <name>SetTarget</name>" +
+	"      <argumentList>" +
+	"        <argument>" +
+	"          <name>newTargetValue</name>" +
+	"          <direction>in</direction>" +
+	"          <relatedStateVariable>Target</relatedStateVariable>" +
+	"        </argument>" +
+	"      </argumentList>" +
+	"    </action>" +
+	"    <action>" +
+	"    <name>GetTarget</name>" +
+	"      <argumentList>" +
+	"        <argument>" +
+	"          <name>RetTargetValue</name>" +
+	"          <direction>out</direction>" +
+	"          <relatedStateVariable>Target</relatedStateVariable>" +
+	"        </argument>" +
+	"      </argumentList>" +
+	"    </action>" +
+	"    <action>" +
+	"    <name>GetStatus</name>" +
+	"      <argumentList>" +
+	"        <argument>" +
+	"          <name>ResultStatus</name>" +
+	"          <direction>out</direction>" +
+	"          <relatedStateVariable>Status</relatedStateVariable>" +
+	"        </argument>" +
+	"      </argumentList>" +
+	"    </action>" +
+	"  </actionList>" +
+	"</scpd>"
