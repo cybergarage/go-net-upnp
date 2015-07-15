24ce18886d6d25d9e980052d285c398226b86222
Satoshi Konno <skonno@cybergarage.org>
2015-07-15 13:24:01 +0900

    * Added url functions.

---
+++ src/net/upnp/util/url_test.go
@@ -0,0 +1,41 @@
+// Copyright 2015 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package util
+
+import (
+	"testing"
+)
+
+const (
+	errorUrlNotInvalid = "url (%s) is not invalid"
+)
+
+func TestInvalidBaseAndPathURL(t *testing.T) {
+	bases := []string{"", "192.168.100.1"}
+	paths := []string{""}
+
+	for _, base := range bases {
+		for _, path := range paths {
+			url, err := GetAbsoluteURLFromBaseAndPath(base, path)
+			if err == nil {
+				t.Errorf(errorUrlNotInvalid, url.String())
+			}
+		}
+	}
+}
+
+func TestValidBaseAndPathURL(t *testing.T) {
+	bases := []string{"http://192.168.100.1/", "http://192.168.100.1"}
+	paths := []string{"/index.html", "index.html"}
+
+	for _, base := range bases {
+		for _, path := range paths {
+			_, err := GetAbsoluteURLFromBaseAndPath(base, path)
+			if err != nil {
+				t.Error(err)
+			}
+		}
+	}
+}
