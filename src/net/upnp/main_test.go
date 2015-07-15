9d7506b93bc432a9e0ec9ebb79d9d2fc85c6f4ec
Satoshi Konno <skonno@cybergarage.org>
2015-07-16 01:16:55 +0900

    * Added TestMain to set the logger.

---
+++ src/net/upnp/main_test.go
@@ -0,0 +1,19 @@
+// Copyright 2015 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package upnp
+
+import (
+	"os"
+	"testing"
+
+	"net/upnp/log"
+)
+
+func TestMain(m *testing.M) {
+	logger := log.NewStdoutLogger(log.LoggerLevelTrace)
+	log.SetSharedLogger(logger)
+
+	os.Exit(m.Run())
+}
