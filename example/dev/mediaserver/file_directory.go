61c4e1be20f3f179b87c4d3ec9e384caa51f6a49
Satoshi Konno <skonno@cybergarage.org>
2015-07-24 12:59:54 +0900

    * Added contents.

---
+++ example/dev/mediaserver/file_directory.go
@@ -0,0 +1,16 @@
+// Copyright 2015 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package media
+
+type FileDirectory struct {
+	*Directory
+}
+
+func NewFileDirectory() *FileDirectory {
+	dir := &FileDirectory{
+		Directory: NewDictionary(),
+	}
+	return dir
+}
